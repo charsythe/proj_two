33a190a0a7ed6900333d621f66914583267d97c6
charsythe <booktearex@gmail.com>
2018-10-16 13:34:51 -0400

    figsize added

--- rw_visual.py
+++ rw_visual.py
@@ -8,6 +8,9 @@ while True:
 	rw = RandomWalk()
 	rw.fill_walk()
 
+	# Set the size of the plotting window.
+	plt.figure(figsize=(10, 6))
+
 	point_numbers = list(range(rw.num_points))
 	plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Greens, 
 		edgecolor='none', s=15)
@@ -20,7 +23,7 @@ while True:
 	# Remove the axes.
 	plt.axes().get_xaxis().set_visible(False)
 	plt.axes().get_yaxis().set_visible(False)
-	
+
 	plt.show()
 
 	keep_running = input("Make another walk? (y/n): ")
